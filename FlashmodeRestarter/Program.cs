19d527decb7ecbd250c98dc282c17445881bbf13
ingen084 <ingen188@gmail.com>
2023-08-22 23:49:07 +0900

    デフォルト値を設定するように

--- FlashmodeRestarter/Program.cs
+++ FlashmodeRestarter/Program.cs
@@ -8,7 +8,7 @@ var devices = new List<SerialDevice>();
 // ベンダーIDが 2E8A(Raspberry Pi) のデバイスを列挙
 foreach (ManagementObject drive in new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_SerialPort").Get())
     if (drive.GetPropertyValue("Caption") is string displayName && drive.GetPropertyValue("DeviceID") is string devId && ((drive.GetPropertyValue("PNPDeviceID") as string)?.Contains("VID_2E8A") ?? false))
-            devices.Add(new SerialDevice(displayName, devId));
+        devices.Add(new SerialDevice(displayName, devId));
 
 if (devices.Count == 0)
 {
@@ -17,10 +17,19 @@ if (devices.Count == 0)
     return;
 }
 
+// 引数で指定されたデバイスがある場合はそれを選択
+var argSelectedDevice = devices.FirstOrDefault(d => args.FirstOrDefault() == d.ComName);
+if (args.Length > 0 && argSelectedDevice == null)
+{
+    Console.WriteLine($"引数で指定されたデバイス ({args[0]}) が見つかりませんでした。");
+    Console.ReadKey();
+    return;
+}
+
 // 複数ある場合は選択させる
-var device = devices[0];
-if (devices.Count > 1)
-    device = Prompt.Select("デバイスが複数あります。選択してください", devices, textSelector: d => d.DisplayName);
+var device = argSelectedDevice ?? devices[0];
+if (argSelectedDevice == null && devices.Count > 1)
+    device = Prompt.Select("デバイスが複数あります。選択してください", devices, defaultValue: devices[0], textSelector: d => d.DisplayName);
 
 // ボーレート 1200 で開いて閉じるとUSBストレージ書き込みモードで再起動する
 using var port = new SerialPort(device.ComName, 1200);
